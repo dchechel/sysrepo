37ece11d3cd74fc1695cd3c735a15daa93f7bade
echdwea <dawid.chechelski@ericsson.com>
2019-11-18 13:36:02 +0100

    sr_free_ctx added

--- src/clientlib/client_library.c
+++ src/clientlib/client_library.c
@@ -3833,12 +3833,15 @@ sr_fd_event_process(int fd, sr_fd_event_t event, sr_fd_change_t **fd_change_set,
 void
 sr_free_ctx (sr_subscription_ctx_t* subscription_ctx)
 {
+    printf("sr_free_ctx()\n");
     for (unsigned int i=0; i<subscription_ctx->sm_subscription_cnt; i++)
     {
+        printf("iteration no. %d\n", i);
         void* tmp = subscription_ctx->sm_subscriptions[i]->private_ctx;
         if (tmp != NULL)
         {
-            free(subscription_ctx->sm_subscriptions[i]->private_ctx);
+            printf("freeing element no. %d\n");
+            free(tmp);
         }
     }
 }
